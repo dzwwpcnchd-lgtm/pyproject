49718cb8743398f39d6f931521a116beb69259ee
dzwwpcnchd-lgtm <dzwwpcnchd@privaterelay.appleid.com>
2025-11-27 18:59:02 +0800

    Update comments and variable names in day2.py

--- day2.py
+++ day2.py
@@ -1,4 +1,4 @@
-#-----黑马第二章------
+#-----heima第二章------
 # int,float,string
 # 字面量  写在代码中固定的值
 # 常见  整数，浮点，字符串
@@ -13,15 +13,15 @@ print('黑马程序员')
 '''
 
 # 变量 记录数据 定义 变量名 = 变量值
-'''money = 12
+money = 12
 print(f'有{money}元')
 print('有',money,'元')
 print('有'+str(money)+'元')
 money = money - 10
 print('花了10元，还剩下', money)
-'''
+
 # 练习
-'''
+
 money = 50
 print('当前钱包余额为：',money,'元')
 print('购买冰淇淋，花费：',10,'元')
@@ -29,9 +29,8 @@ money = money -10
 print('购买可乐，花费：',5,'元')
 money = money -5
 print('最终剩余：',money,'元')
-''' 
+
 # type()查看数据类型
-'''
 print(type(123))
 print(type('bihao'))
 print(type(12.3))
@@ -39,6 +38,7 @@ print(type(12.3))
 int_type = type(13)
 print(int_type)
 print(type(int_type))
+
 #
 name= 'heima'
 name_type=type(name)
@@ -58,15 +58,12 @@ print(type(num3),num3)
 # 浮点型转换整数
 int_num = int(11.2)
 print(type(int_num),int_num) 四舍五入
-'''
-'''
+
 标识符  编写代码时，对变量，类，方法编写的名字
 1.只能使用中文，英文，数字，下划线，数字不可以开头
 2.大小写敏感
 3.不可以使用关键字
-'''
 
-'''
 运算符
 数学运算符
 + - * /
@@ -95,7 +92,7 @@ num **= 2
 print('num = num ** 2',num)
 num //= 2
 print('num = num // 2',num)
-'''
+
 # 字符串拓展
 # 定义 
 # name = '1';name = "2"; name = ''' 3'''
@@ -153,7 +150,7 @@ print(f'1*2的结果是{1*2}')
 print('字符串在python中的类型名是%s' % type('字符串') )
 
 #综合练习
-name = '传智播客'
+name = '传智播'
 stock_price = 19.99
 stock_code = '003032'
 stock_price_daily_growth_factor = 1.2
@@ -161,21 +158,21 @@ growth_days = 7
 print(f'公司：{name},股票代码：{stock_code},当前股价：{stock_price}')
 print('每日增长系数是：%.1f,经过%d天的增长后，股价达到了：%.2f'
        %(stock_price_daily_growth_factor,growth_days,stock_price*stock_price_daily_growth_factor**7))
-'''
+
 #数据输入 input()语句
 name = input("告诉我你是谁")
 print('我知道了，你是：%s' %name )
 
 num= input("告诉我你的银行卡密码")
 print("你的银行卡密码类型是",type(num)) #无论输入什么类型数据，input接收类型是字符串类型
-'''
+
 #练习
-'''
+
 user_name = input('输入名字')
 user_type = 'SSSSSVIP'
 print(f'您好：{user_name},您是尊贵的：{user_type}用户，欢迎您的光临。')
-'''
-'''
+
+
 #Bmi计算器
 try :
     height = float(input('输入身高'))
@@ -192,7 +189,7 @@ try :
     print('身高为{height},体重为：{weight}，健康等级为：{level}'.format(height=height,weight=weight,level=level))
 except ValueError :
     print("请输入正确的数据类型")
-'''
+
 #闰年
 age = int(input("输入年份："))
 if age % 4 == 0:
